09c5cf1450e188cbcea963603656ad3c225d7a48
maribelseara <mseara@student.42barcelona.com>
2023-02-14 11:42:03 +0100

    included median.c testing pending

---
+++ median.c
@@ -0,0 +1,53 @@
+# include "push_swap.h"
+
+int *ft_lst_to_array(it_list stackx, int items)
+{
+    int *sorted;
+    int i;
+
+    sorted = malloc(sizeof(int) * items);
+    i = 0;
+    while (i < items)
+    {
+        sorted[i++]=stackx->content;
+        stackx = stackx->next;
+    }
+}
+
+int *ft_get_sorted_array (it_list stackx, int items)
+{
+    int *sorted;
+    int i;
+    int j;
+    int tmp;
+
+    sorted = ft_lst_to_array(stackx, items);
+    i = 0;
+    while (i < items - 1)
+    {
+        j = 1;
+        while (j < items)
+        {
+            if (array[j - 1] > array[j])
+            {
+                tmp = array[j];
+                array[j] = array[j - 1];
+                array[j - 1]=tmp;
+            }
+            j++;
+        }
+        i++;
+    }
+    return (sorted);
+}
+
+int ft_get_median(it_list stackx)
+{
+    int items;
+    int *sorted;
+
+    items = ft_count_lst_items(stackx);
+    sorted = ft_get_sorted_array(stackx, items);
+    median = sorted[items/2];
+    return (median);
+}
